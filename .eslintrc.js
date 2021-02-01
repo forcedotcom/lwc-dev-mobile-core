a90da5577da2059df3304aa7814c241a6517dac4
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2021-02-01 13:53:18 -0800

    move common files over to here

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        // 'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:prettier/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    plugins: ['prettier', '@typescript-eslint'],
+    parserOptions: {
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+    rules: {
+        'prettier/prettier': 'warn',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'no-unused-vars': 'off',
+    },
+};
