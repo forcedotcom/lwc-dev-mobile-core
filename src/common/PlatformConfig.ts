915c36e86728efe84422db9669a045bba6c0799b
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2021-02-22 09:48:54 -0800

    testing

--- src/common/PlatformConfig.ts
+++ src/common/PlatformConfig.ts
@@ -27,6 +27,7 @@
  * (i.e import iOSConfig from { IOSConfig.json } or similar for Android)
  */
 
+// testing
 export class PlatformConfig {
     public static iOSConfig(): IOSConfig {
         return new IOSConfig();
