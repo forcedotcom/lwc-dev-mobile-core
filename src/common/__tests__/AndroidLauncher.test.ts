1229737470c80db10fa2a467024cc9dcc99df478
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2021-03-18 15:00:45 -0700

    a few more mocks

--- src/common/__tests__/AndroidLauncher.test.ts
+++ src/common/__tests__/AndroidLauncher.test.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import path from 'path';
 import { CommonUtils } from '../CommonUtils';
 import { AndroidLauncher } from '../AndroidLauncher';
-import { AndroidUtils } from '../AndroidUtils';
+import { AndroidSDKRootSource, AndroidUtils } from '../AndroidUtils';
 import { AndroidMockData } from './AndroidMockData';
 
 const myCommandBlockMock = jest.fn(
@@ -17,10 +18,24 @@ const myCommandBlockMock = jest.fn(
         })
 );
 
+const mockAndroidHome = '/mock-android-home';
+const mockCmdLineToolsBin = path.normalize(
+    path.join(mockAndroidHome, 'cmdline-tools', 'latest', 'bin')
+);
+
 describe('Android Launcher tests', () => {
     beforeEach(() => {
         // tslint:disable-next-line: no-empty
         jest.spyOn(CommonUtils, 'startCliAction').mockImplementation(() => {});
+        jest.spyOn(AndroidUtils, 'getAndroidSdkRoot').mockImplementation(() => {
+            return {
+                rootLocation: mockAndroidHome,
+                rootSource: AndroidSDKRootSource.androidHome
+            };
+        });
+        jest.spyOn(AndroidUtils, 'getAndroidCmdLineToolsBin').mockReturnValue(
+            mockCmdLineToolsBin
+        );
     });
 
     afterEach(() => {
