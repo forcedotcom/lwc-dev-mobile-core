bf6808d5ee73bc2f22000e32c652380eb96715cd
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2023-02-23 14:42:25 -0800

    add more comments

--- src/common/BaseCommand.ts
+++ src/common/BaseCommand.ts
@@ -69,6 +69,14 @@ export abstract class BaseCommand
         const flags = (<typeof SfCommand>this.constructor).flags;
         const flagEntries = Object.entries(flags);
         flagEntries.forEach((flag) => {
+            // Object.entries returns an array of KeyValue pairs where item[0]
+            // is key and item[1] is value. In this context flag[1] holds another
+            // object which defines the configs for that flag. oclif adds a default
+            // parser to flag configs for all flags. This parser just accepts any value
+            // that is passed in for that flag without validating anything.
+            // Here we check to see if validate is added to the configs for a flag.
+            // If so then we override the default parser with our parser which runs
+            // the validation check and only accepts the flag value if validation passes.
             const configEntries = Object.entries(flag[1]);
             const hasValidate = configEntries.find(
                 (keyValuePair) => keyValuePair[0] === 'validate'
