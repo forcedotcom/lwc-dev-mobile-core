a4ff84311523a1d844095c5b876ae9929fe3b4ea
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2023-10-26 10:17:51 -0700

    re-export everything

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,24 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-export default {};
+
+export * from './cli/commands/force/lightning/local/setup';
+
+export * from './common/AndroidEnvironmentRequirements';
+export * from './common/AndroidLauncher';
+export * from './common/AndroidTypes';
+export * from './common/AndroidUtils';
+export * from './common/BaseCommand';
+export * from './common/Common';
+export * from './common/CommonUtils';
+export * from './common/IOSEnvironmentRequirements';
+export * from './common/IOSLauncher';
+export * from './common/IOSTypes';
+export * from './common/IOSUtils';
+export * from './common/LoggerSetup';
+export * from './common/MacNetworkUtils';
+export * from './common/PerformanceMarkers';
+export * from './common/PlatformConfig';
+export * from './common/PreviewConfigFile';
+export * from './common/PreviewUtils';
+export * from './common/Requirements';
