3cea949a75103f804fe16f7f70ec0394b9c6ed20
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2024-05-13 08:47:35 -0700

    fix: update deps to fix up packaging issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: ['eslint-config-salesforce-typescript', 'plugin:sf-plugin/recommended'],
-  root: true,
-  rules: {
-    header: 'off',
-  }
+    extends: ['eslint-config-salesforce-typescript', 'plugin:sf-plugin/recommended'],
+    ignorePatterns: ['*.cjs'],
+    root: true,
+    rules: {
+        header: 'off'
+    }
 };
